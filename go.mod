93875866b71558f53370a83703d1323336a73189
Jachym Solecky <j.solecky@rewe-group.at>
2026-02-21 23:44:54 +0100

    Clean comments in main.go and fix imports

--- go.mod
+++ go.mod
@@ -3,14 +3,14 @@ module github.com/jachymsol/cert-manager-webhook
 go 1.25.0
 
 require (
-	github.com/cert-manager/cert-manager v1.19.3
+	github.com/cert-manager/cert-manager v1.19.2
 	github.com/miekg/dns v1.1.68
 	github.com/stretchr/testify v1.11.1
 	golang.org/x/net v0.48.0
-	k8s.io/api v0.35.1
 	k8s.io/apiextensions-apiserver v0.34.1
-	k8s.io/apimachinery v0.35.1
-	k8s.io/client-go v0.35.1
+	k8s.io/apimachinery v0.34.1
+	k8s.io/client-go v0.34.1
+	k8s.io/klog/v2 v2.130.1
 )
 
 require (
@@ -62,9 +62,9 @@ require (
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/stoewer/go-strcase v1.3.1 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
-	go.etcd.io/etcd/api/v3 v3.6.5 // indirect
-	go.etcd.io/etcd/client/pkg/v3 v3.6.5 // indirect
-	go.etcd.io/etcd/client/v3 v3.6.5 // indirect
+	go.etcd.io/etcd/api/v3 v3.6.4 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.6.4 // indirect
+	go.etcd.io/etcd/client/v3 v3.6.4 // indirect
 	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.61.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.61.0 // indirect
@@ -77,18 +77,18 @@ require (
 	go.opentelemetry.io/proto/otlp v1.7.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
-	go.yaml.in/yaml/v2 v2.4.3 // indirect
+	go.yaml.in/yaml/v2 v2.4.2 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/crypto v0.47.0 // indirect
+	golang.org/x/crypto v0.46.0 // indirect
 	golang.org/x/exp v0.0.0-20250718183923-645b1fa84792 // indirect
-	golang.org/x/mod v0.31.0 // indirect
+	golang.org/x/mod v0.30.0 // indirect
 	golang.org/x/oauth2 v0.31.0 // indirect
 	golang.org/x/sync v0.19.0 // indirect
-	golang.org/x/sys v0.40.0 // indirect
-	golang.org/x/term v0.39.0 // indirect
-	golang.org/x/text v0.33.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/term v0.38.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
 	golang.org/x/time v0.13.0 // indirect
-	golang.org/x/tools v0.40.0 // indirect
+	golang.org/x/tools v0.39.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.5.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250721164621-a45f3dfb1074 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250929231259-57b25ae835d4 // indirect
@@ -98,12 +98,12 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/apiserver v0.35.1 // indirect
-	k8s.io/component-base v0.35.1 // indirect
-	k8s.io/klog/v2 v2.130.1 // indirect
-	k8s.io/kms v0.35.1 // indirect
+	k8s.io/api v0.34.1 // indirect
+	k8s.io/apiserver v0.34.1 // indirect
+	k8s.io/component-base v0.34.1 // indirect
+	k8s.io/kms v0.34.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20250910181357-589584f1c912 // indirect
-	k8s.io/utils v0.0.0-20251002143259-bc988d571ff4 // indirect
+	k8s.io/utils v0.0.0-20250820121507-0af2bda4dd1d // indirect
 	sigs.k8s.io/apiserver-network-proxy/konnectivity-client v0.33.0 // indirect
 	sigs.k8s.io/controller-runtime v0.22.3 // indirect
 	sigs.k8s.io/gateway-api v1.4.0 // indirect
--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 	"os"
 	"strings"
 
-	corev1 "k8s.io/api/core/v1"
 	extapi "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/klog/v2"
 
 	acmev1alpha1 "github.com/cert-manager/cert-manager/pkg/acme/webhook/apis/acme/v1alpha1"
 	"github.com/cert-manager/cert-manager/pkg/acme/webhook/cmd"
@@ -24,85 +24,48 @@ func main() {
 		panic("GROUP_NAME must be specified")
 	}
 
-	// This will register our custom DNS provider with the webhook serving
-	// library, making it available as an API under the provided GroupName.
-	// You can register multiple DNS provider implementations with a single
-	// webhook, where the Name() method will be used to disambiguate between
-	// the different implementations.
 	cmd.RunWebhookServer(GroupName,
 		&jachymsolDNSProviderSolver{},
 	)
 }
 
-// jachymsolDNSProviderSolver implements the provider-specific logic needed to
-// 'present' an ACME challenge TXT record for your own DNS provider.
-// To do so, it must implement the `github.com/cert-manager/cert-manager/pkg/acme/webhook.Solver`
-// interface.
 type jachymsolDNSProviderSolver struct {
-	// If a Kubernetes 'clientset' is needed, you must:
-	// 1. uncomment the additional `client` field in this structure below
-	// 2. uncomment the "k8s.io/client-go/kubernetes" import at the top of the file
-	// 3. uncomment the relevant code in the Initialize method below
-	// 4. ensure your webhook's service account has the required RBAC role
-	//    assigned to it for interacting with the Kubernetes APIs you need.
 	client *kubernetes.Clientset
 }
 
-// jachymsolDNSProviderConfig is a structure that is used to decode into when
-// solving a DNS01 challenge.
-// This information is provided by cert-manager, and may be a reference to
-// additional configuration that's needed to solve the challenge for this
-// particular certificate or issuer.
-// This typically includes references to Secret resources containing DNS
-// provider credentials, in cases where a 'multi-tenant' DNS solver is being
-// created.
-// If you do *not* require per-issuer or per-certificate configuration to be
-// provided to your webhook, you can skip decoding altogether in favour of
-// using CLI flags or similar to provide configuration.
-// You should not include sensitive information here. If credentials need to
-// be used by your provider here, you should reference a Kubernetes Secret
-// resource and fetch these credentials using a Kubernetes clientset.
 type jachymsolDNSProviderConfig struct {
-	// Change the two fields below according to the format of the configuration
-	// to be decoded.
-	// These fields will be set by users in the
-	// `issuer.spec.acme.dns01.providers.webhook.config` field.
-
-	BaseUrl  string           `json:"baseUrl,omitempty"`
+	BaseUrl  string           `json:"baseUrl"`
 	Username valueOrSecretRef `json:"username"`
 	Password valueOrSecretRef `json:"password"`
 	DomainId valueOrSecretRef `json:"domainId"`
 }
 
 type valueOrSecretRef struct {
-	Value     string                    `json:"value,omitempty"`
-	SecretRef *corev1.SecretKeySelector `json:"secretRef,omitempty"`
+	Value     string     `json:"value,omitempty"`
+	SecretRef *secretRef `json:"secretRef,omitempty"`
 }
 
-// Name is used as the name for this DNS solver when referencing it on the ACME
-// Issuer resource.
-// This should be unique **within the group name**, i.e. you can have two
-// solvers configured with the same Name() **so long as they do not co-exist
-// within a single webhook deployment**.
-// For example, `cloudflare` may be used as the name of a solver.
+type secretRef struct {
+	Name string `json:"name"`
+	Key  string `json:"key"`
+}
+
 func (c *jachymsolDNSProviderSolver) Name() string {
 	return "jachymsol"
 }
 
-// Present is responsible for actually presenting the DNS record with the
-// DNS provider.
-// This method should tolerate being called multiple times with the same value.
-// cert-manager itself will later perform a self check to ensure that the
-// solver has correctly configured the DNS provider.
 func (c *jachymsolDNSProviderSolver) Present(ch *acmev1alpha1.ChallengeRequest) error {
-	cfg, err := loadConfig(ch.Config)
+	klog.V(6).Infof("call function Present: namespace=%s, zone=%s, fqdn=%s",
+		ch.ResourceNamespace, ch.ResolvedZone, ch.ResolvedFQDN)
+
+	config, err := loadConfig(ch.Config)
 	if err != nil {
-		return fmt.Errorf("Unable to load config: %w", err)
+		return fmt.Errorf("unable to load config: %w", err)
 	}
 
-	client, err := c.getClient(cfg, ch.ResourceNamespace)
+	client, err := c.getClient(config, ch.ResourceNamespace)
 	if err != nil {
-		return fmt.Errorf("Unable to get client: %w", err)
+		return fmt.Errorf("unable to get client: %w", err)
 	}
 
 	// Extract subdomain from FQDN (e.g., _acme-challenge.sub.example.com -> _acme-challenge.sub)
@@ -110,52 +73,53 @@ func (c *jachymsolDNSProviderSolver) Present(ch *acmev1alpha1.ChallengeRequest) 
 	sub := strings.Join(domainParts[:len(domainParts)-2], ".")
 
 	// Publish the DNS record
-	return client.PublishRecord(sub, ch.Key)
-}
-
-// CleanUp should delete the relevant TXT record from the DNS provider console.
-// If multiple TXT records exist with the same record name (e.g.
-// _acme-challenge.example.com) then **only** the record with the same `key`
-// value provided on the ChallengeRequest should be cleaned up.
-// This is in order to facilitate multiple DNS validations for the same domain
-// concurrently.
-func (c *jachymsolDNSProviderSolver) CleanUp(ch *acmev1alpha1.ChallengeRequest) error {
-	cfg, err := loadConfig(ch.Config)
+	err = client.PublishRecord(sub, ch.Key)
 	if err != nil {
-		return fmt.Errorf("Unable to load config: %w", err)
+		return fmt.Errorf("unable to publish record: %w", err)
 	}
 
-	client, err := c.getClient(cfg, ch.ResourceNamespace)
-	if err != nil {
-		return fmt.Errorf("Unable to get client: %w", err)
-	}
-
-	// Delete the DNS record
-	return client.DeleteRecord(ch.ResolvedFQDN)
-}
-
-// Initialize will be called when the webhook first starts.
-// This method can be used to instantiate the webhook, i.e. initialising
-// connections or warming up caches.
-// Typically, the kubeClientConfig parameter is used to build a Kubernetes
-// client that can be used to fetch resources from the Kubernetes API, e.g.
-// Secret resources containing credentials used to authenticate with DNS
-// provider accounts.
-// The stopCh can be used to handle early termination of the webhook, in cases
-// where a SIGTERM or similar signal is sent to the webhook process.
-func (c *jachymsolDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
-	cl, err := kubernetes.NewForConfig(kubeClientConfig)
-	if err != nil {
-		return err
-	}
-
-	c.client = cl
+	klog.Infof("Presented txt record %v", ch.ResolvedFQDN)
 
 	return nil
 }
 
-// loadConfig is a small helper function that decodes JSON configuration into
-// the typed config struct.
+func (c *jachymsolDNSProviderSolver) CleanUp(ch *acmev1alpha1.ChallengeRequest) error {
+	klog.V(6).Infof("call function CleanUp: namespace=%s, zone=%s, fqdn=%s",
+		ch.ResourceNamespace, ch.ResolvedZone, ch.ResolvedFQDN)
+
+	config, err := loadConfig(ch.Config)
+	if err != nil {
+		return fmt.Errorf("unable to load config: %w", err)
+	}
+
+	client, err := c.getClient(config, ch.ResourceNamespace)
+	if err != nil {
+		return fmt.Errorf("unable to get client: %w", err)
+	}
+
+	// Delete the DNS record
+	err = client.DeleteRecord(ch.ResolvedFQDN)
+	if err != nil {
+		return fmt.Errorf("unable to delete record: %w", err)
+	}
+
+	klog.Infof("Deleted TXT record result: %v", string(ch.ResolvedFQDN))
+
+	return nil
+}
+
+func (c *jachymsolDNSProviderSolver) Initialize(kubeClientConfig *rest.Config, stopCh <-chan struct{}) error {
+	k8sClient, err := kubernetes.NewForConfig(kubeClientConfig)
+	klog.V(6).Infof("Input variable stopCh is %d length", len(stopCh))
+	if err != nil {
+		return err
+	}
+
+	c.client = k8sClient
+
+	return nil
+}
+
 func loadConfig(cfgJSON *extapi.JSON) (jachymsolDNSProviderConfig, error) {
 	cfg := jachymsolDNSProviderConfig{}
 	// handle the 'base case' where no configuration has been provided
@@ -169,7 +133,6 @@ func loadConfig(cfgJSON *extapi.JSON) (jachymsolDNSProviderConfig, error) {
 	return cfg, nil
 }
 
-// getClient is a helper function to create the DnsClient
 func (c *jachymsolDNSProviderSolver) getClient(cfg jachymsolDNSProviderConfig, namespace string) (client *DnsClient, err error) {
 	username, err := c.resolveValueOrFromSecret(cfg.Username, namespace)
 	if err != nil {
@@ -197,14 +160,12 @@ func (c *jachymsolDNSProviderSolver) getClient(cfg jachymsolDNSProviderConfig, n
 	return dnsClient, nil
 }
 
-// resolveValueOrFromSecret is a helper function which resolves the value from secret reference
-// if it is set there
 func (c *jachymsolDNSProviderSolver) resolveValueOrFromSecret(vsr valueOrSecretRef, namespace string) (value string, err error) {
 	if vsr.SecretRef != nil {
 		secret, err := c.client.CoreV1().Secrets(namespace).Get(context.Background(), vsr.SecretRef.Name, metav1.GetOptions{})
 
 		if err != nil {
-			return "", err
+			return "", fmt.Errorf("unable to get secret %s: %w", vsr.SecretRef.Name, err)
 		}
 
 		secBytes, ok := secret.Data[vsr.SecretRef.Key]
